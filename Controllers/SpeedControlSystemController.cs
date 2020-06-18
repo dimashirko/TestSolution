1774cacc71606029e04ac79a2aa9b9c2d4c19a8c
dimas <dimas@DESKTOP-APL9TT5>
2020-06-18 22:00:06 +0300

    подготовка

---
+++ Controllers/SpeedControlSystemController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace TestSolution.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SpeedControlSystemController : ControllerBase
+    {
+        private readonly ILogger<SpeedControlSystemController> _logger;
+
+        public SpeedControlSystemController(ILogger<SpeedControlSystemController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+
+        }
+    }
+}
