8a7a9ed329f0669e77f4e165eea0b4442ff51715
dimas <dimas@DESKTOP-APL9TT5>
2020-06-18 22:18:49 +0300

    Base system files

---
+++ SpeedInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SpeedControlSystemService
+{
+    /// <summary>
+    /// Describes fixed speed info
+    /// </summary>
+    public class SpeedInfo
+    {
+        /// <summary>
+        /// Fixing dates
+        /// </summary>
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// Fixed auto license plate
+        /// </summary>
+        public string LicensePlate { get; set; }
+
+        /// <summary>
+        /// Fixed speed
+        /// </summary>
+        public float Speed { get; set; }
+    }
+}
